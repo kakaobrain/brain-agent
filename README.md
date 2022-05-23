c47fecde9ac92dcaa6195456e88d70c7ca8f7140
allan.kim01 <allan.kim01@kakaobrain.com>
2022-05-23 10:42:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,129 @@
+# Brain Agent
+***Brain Agent*** is a distributed agent learning system for large-scale and multi-task reinforcement learning, developed by [Kakao Brain](https://www.kakaobrain.com/). 
+Brain Agent is based on the V-trace actor-critic framework [IMPALA](https://arxiv.org/abs/1802.01561) and modifies [Sample Factory](https://github.com/alex-petrenko/sample-factory) to learn a policy network using multiple GPUs and CPUs with a high throughput rate during training.
+
+Especially, Brain Agent supports the [PopArt](https://arxiv.org/abs/1809.04474) normalization as well as the [TransformerXL-I](https://proceedings.mlr.press/v119/parisotto20a.html) core of the policy network. In addition, for fast and stable training, it allows to make use of the reconstruction loss by applying the auxiliary decoder to the encoder of the policy network.
+
+With these advanced techniques Brain Agent obtains the state-of-the-art result on [DeepMindLab](https://github.com/deepmind/lab) benchmark (DMLab-30 multi-task training) in terms of human normalized score after training for 20 billion frames.
+In specific, our transformer-based policy network has 28 million parameters and was trained on 16 V100 GPUs for 7 days.
+
+We release the source code and trained agent to facilitate future research on DMLab benchmark.
+
+The agent successfully obtained SOTA score of **91.25** (testing) in terms of capped human normalized score on DMLab-30 
+(multi-task mode), after training for 20B frames.
+
+
+## Installation
+This code has been developed under Python 3.7, Pytorch 1.9.0 and CUDA 11.1.
+For DMLab environment, please follow installation instructions from [DMLab Github](https://github.com/deepmind/lab/blob/master/docs/users/build.md).
+
+Please run the following command to install the other necessary dependencies.
+```bash
+pip install -r requirements.txt
+```
+
+
+## Training
+The script `train.py` is used for training. Example usage to train a model on DMLab-30 with our 28M 
+transformer-based policy network on 4 nodes having 4 GPUs on each node is
+```bash
+sleep 120; python -m dist_launch --nnodes=4 --node_rank=0 --nproc_per_node=4 --master_addr=$MASTER_ADDR -m train \ 
+  cfg=configs/trxl_recon_train.yaml train_dir=$TRAIN_DIR experiment=EXPERIMENT_DIR 
+sleep 120; python -m dist_launch --nnodes=4 --node_rank=1 --nproc_per_node=4 --master_addr=$MASTER_ADDR -m train \ 
+  cfg=configs/trxl_recon_train.yaml train_dir=$TRAIN_DIR experiment=EXPERIMENT_DIR
+sleep 120; python -m dist_launch --nnodes=4 --node_rank=2 --nproc_per_node=4 --master_addr=$MASTER_ADDR -m train \ 
+  cfg=configs/trxl_recon_train.yaml train_dir=$TRAIN_DIR experiment=EXPERIMENT_DIR
+sleep 120; python -m dist_launch --nnodes=4 --node_rank=3 --nproc_per_node=4 --master_addr=$MASTER_ADDR -m train \ 
+  cfg=configs/trxl_recon_train.yaml train_dir=$TRAIN_DIR experiment=EXPERIMENT_DIR
+```
+
+## Evaluation
+### Trained Agent on DMLab-30
+We provide the checkpoint of our trained agent on DMLab-30. The overall performances of our agent and the previous
+state-of-the-art models are as follows.
+
+|  Model | Mean HNS  | Median HNS | Mean Capped HNS |
+|:----------:|:--------:|:----------:|:---------------:|
+| [MERLIN](https://arxiv.org/pdf/1803.10760.pdf) | 115.2  | - | 89.4 |
+| [GTrXL](https://proceedings.mlr.press/v119/parisotto20a.html) | 117.6 | - | 89.1 |
+| [CoBERL](https://arxiv.org/pdf/2107.05431.pdf) | 115.47  | 110.86 |- |
+| [R2D2+](https://openreview.net/pdf?id=r1lyTjAqYX) | -  | 99.5 | 85.7 |
+| [LASER](https://arxiv.org/abs/1909.11583) | -  | 97.2 | 81.7 |
+| [PBL](https://arxiv.org/pdf/2004.14646.pdf) | 104.16  | - | 81.5 |
+| [PopArt-IMPALA](https://arxiv.org/abs/1809.04474) | -  | - | 72.8 |
+| [IMPALA](https://arxiv.org/abs/1802.01561) | -  | - | 58.4 |
+| Ours ([20B checkpoint](https://arena.kakaocdn.net/brainrepo/models/brain_agent/84ac1c594b8eb95e7fd9879d6172f99b/trxl_recon_20b.pth)) | 123.60 ± 0.84 | 108.63	± 1.20 | **91.25 ± 0.41**|
+
+Our results were obtained by 3 runs with different random seeds where each run carried out 100 episodes for each task (level).
+
+The detailed break down HNS scores by our agent are as follows:
+
+| Level | HNS |
+|:-----:|:---:|
+rooms_collect_good_objects_(train / test)		|	97.58	±   0.20 / 89.39	±	1.42		|
+rooms_exploit_deferred_effects_(train / test)		|	38.86	±   3.48 / 4.04	±	0.89		|
+rooms_select_nonmatching_object		|	99.52	±	0.97		|
+rooms_watermaze		|	111.20	±	2.29		|
+rooms_keys_doors_puzzle		|	61.24	±	9.09		|
+language_select_described_object		|	155.35	±	0.17		|
+language_select_located_object		|	252.04	±	0.31		|
+language_execute_random_task		|	145.21	±	0.36		|
+language_answer_quantitative_question		|	163.72	±	1.36		|
+lasertag_one_opponent_small		|	249.99	±	6.64		|
+lasertag_three_opponents_small		|	246.68	±	5.99		|
+lasertag_one_opponent_large		|	82.55	±	2.15		|
+lasertag_three_opponents_large		|	96.54	±	0.67		|
+natlab_fixed_large_map		|	120.53	±	1.79		|
+natlab_varying_map_regrowth		|	108.14	±	1.25		|
+natlab_varying_map_randomized		|	85.53	±	6.69		|
+skymaze_irreversible_path_hard		|	61.63	±	2.52		|
+skymaze_irreversible_path_varied		|	81.31	±	2.34		|
+psychlab_arbitrary_visuomotor_mapping		|	101.82	±	0.19		|
+psychlab_continuous_recognition		|	102.46	±	0.32		|
+psychlab_sequential_comparison		|	75.74	±	0.58		|
+psychlab_visual_search		|	101.91	±	0.00		|
+explore_object_locations_small		|	123.54	±	2.61		|
+explore_object_locations_large		|	115.43	±	1.64		|
+explore_obstructed_goals_small		|	166.75	±	3.63		|
+explore_obstructed_goals_large		|	153.44	±	3.20		|
+explore_goal_locations_small		|	177.16	±	0.37		|
+explore_goal_locations_large		|	160.39	±	3.32		|
+explore_object_rewards_few		|	109.58	±	3.53		|
+explore_object_rewards_many		|	105.15	±	0.75		|
+
+
+- Learning curves of our 20B checkpoint on dmlab-30
+<div align="center">
+ <img width="800" alt="Learning Curve" src="assets/learning_curve.png"/>
+</div>
+
+
+### Evaluation Command
+The script `eval.py` is used for evaluation. Example usage to test the provided checkpoint, which is located in $CHECKPOINT_FILE_PATH, is
+
+```bash
+python eval.py cfg=configs/trxl_recon_eval.yaml train_dir=$TRAIN_DIR experiment=$EXPERIMENT_DIR model.checkpoint=$CHECKPOINT_FILE_PATH 
+```
+
+## Acknowledgement
+
+The code for overall distributed training is based on the [Sample Factory](alex-petrenko/sample-factory) repo, and the TransformerXL-I code is based on the [Transformer-XL](https://github.com/kimiyoung/transformer-xl) repo.
+
+## Licenses
+This repository is released under the MIT license, included [here](LICENSE).
+
+This repository includes some codes from [sample-factory](https://github.com/alex-petrenko/sample-factory) 
+(MIT license) and [transformer-xl](https://github.com/kimiyoung/transformer-xl) (Apache 2.0 License).
+
+## Contact
+
+If you have any question or feedback regarding this repository, please email to contact@kakaobrain.com
+
+## Citation
+
+If you use Brain Agent in your research or work, please cite it as:
+```
+@misc{kakaobrain2022brain_agent, title = {Brain Agent}, 
+author = {Donghoon Lee, Taehwan Kwon, Seungeun Rho, Daniel Wontae Nam, Jongmin Kim, Daejin Jo, and Sungwoong Kim}, 
+year = {2022}, howpublished = {\url{https://github.com/kakaobrain/brain_agent}} }
+```
---
+++ brain_agent/core/models/transformer.py
@@ -0,0 +1,441 @@
+"""
+Copyright 2019 kimiyoung
+Copyright 2022 Kakao Brain Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Note:
+    This specific file is modification of kimiyoung's version of TrXL
+    https://github.com/kimiyoung/transformer-xl which implements TrXL-I in https://arxiv.org/pdf/1910.06764.pdf for
+    RL environments.
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, demb):
+        super(PositionalEmbedding, self).__init__()
+
+        self.demb = demb
+
+        inv_freq = 1 / (10000 ** (torch.arange(0.0, demb, 2.0) / demb))
+        self.register_buffer('inv_freq', inv_freq)
+
+    def forward(self, pos_seq, bsz=None):
+        sinusoid_inp = torch.ger(pos_seq, self.inv_freq)
+        pos_emb = torch.cat([sinusoid_inp.sin(), sinusoid_inp.cos()], dim=-1)
+
+        if bsz is not None:
+            return pos_emb[:,None,:].expand(-1, bsz, -1)
+        else:
+            return pos_emb[:,None,:]
+
+
+class PositionwiseFF(nn.Module):
+    def __init__(self, d_model, d_inner, pre_lnorm=False):
+        super(PositionwiseFF, self).__init__()
+
+        self.d_model = d_model
+        self.d_inner = d_inner
+
+        self.CoreNet = nn.Sequential(
+            nn.Linear(d_model, d_inner), nn.ReLU(inplace=True),
+            nn.Linear(d_inner, d_model),
+        )
+
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.pre_lnorm = pre_lnorm
+
+    def forward(self, inp):
+        if self.pre_lnorm:
+            ##### layer normalization + positionwise feed-forward
+            core_out = self.CoreNet(self.layer_norm(inp))
+
+            ##### residual connection
+            output = F.relu(core_out) + inp
+        else:
+            ##### positionwise feed-forward
+            core_out = self.CoreNet(inp)
+
+            ##### residual connection + layer normalization
+            output = self.layer_norm(inp + core_out)
+
+        return output
+
+
+class RelPartialLearnableDecoderLayer(nn.Module):
+    def __init__(self, n_head, d_model, d_head, d_inner, **kwargs):
+        super(RelPartialLearnableDecoderLayer, self).__init__()
+
+        self.dec_attn = RelPartialLearnableMultiHeadAttn(n_head, d_model,
+                            d_head, **kwargs)
+        self.pos_ff = PositionwiseFF(d_model, d_inner, pre_lnorm=kwargs.get('pre_lnorm'))
+
+    def forward(self, dec_inp, r, r_w_bias, r_r_bias, dec_attn_mask=None, mems=None):
+
+        output = self.dec_attn(dec_inp, r, r_w_bias, r_r_bias,
+                               attn_mask=dec_attn_mask,
+                               mems=mems)
+        output = self.pos_ff(output)
+
+        return output
+
+
+class RelMultiHeadAttn(nn.Module):
+    def __init__(self, n_head, d_model, d_head, mem_len=None, pre_lnorm=False):
+        super(RelMultiHeadAttn, self).__init__()
+
+        self.n_head = n_head
+        self.d_model = d_model
+        self.d_head = d_head
+
+        self.qkv_net = nn.Linear(d_model, 3 * n_head * d_head, bias=False)
+
+        self.o_net = nn.Linear(n_head * d_head, d_model, bias=False)
+
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.scale = 1 / (d_head ** 0.5)
+
+        self.pre_lnorm = pre_lnorm
+
+    def _parallelogram_mask(self, h, w, left=False):
+        mask = torch.ones((h, w)).bool()
+        m = min(h, w)
+        mask[:m,:m] = torch.triu(mask[:m,:m])
+        mask[-m:,-m:] = torch.tril(mask[-m:,-m:])
+
+        if left:
+            return mask
+        else:
+            return mask.flip(0)
+
+    def _shift(self, x, qlen, klen, mask, left=False):
+        if qlen > 1:
+            zero_pad = torch.zeros((x.size(0), qlen-1, x.size(2), x.size(3)),
+                                    device=x.device, dtype=x.dtype)
+        else:
+            zero_pad = torch.zeros(0, device=x.device, dtype=x.dtype)
+
+        if left:
+            mask = mask.flip(1)
+            x_padded = torch.cat([zero_pad, x], dim=1).expand(qlen, -1, -1, -1)
+        else:
+            x_padded = torch.cat([x, zero_pad], dim=1).expand(qlen, -1, -1, -1)
+
+        x = x_padded.masked_select(mask[:,:,None,None]) \
+                    .view(qlen, klen, x.size(2), x.size(3))
+
+        return x
+
+    def _rel_shift(self, x, zero_triu=False):
+        zero_pad = torch.zeros((x.size(0), 1, *x.size()[2:]),
+                               device=x.device, dtype=x.dtype)
+        x_padded = torch.cat([zero_pad, x], dim=1)
+
+        x_padded = x_padded.view(x.size(1) + 1, x.size(0), *x.size()[2:])
+
+        x = x_padded[1:].view_as(x)
+
+        if zero_triu:
+            ones = torch.ones((x.size(0), x.size(1)))
+            x = x * torch.tril(ones, x.size(1) - x.size(0))[:,:,None,None]
+
+        return x
+
+    def forward(self, w, r, attn_mask=None, mems=None):
+        raise NotImplementedError
+
+
+class RelPartialLearnableMultiHeadAttn(RelMultiHeadAttn):
+    def __init__(self, *args, **kwargs):
+        super(RelPartialLearnableMultiHeadAttn, self).__init__(*args, **kwargs)
+
+        self.r_net = nn.Linear(self.d_model, self.n_head * self.d_head, bias=False)
+
+
+    def forward(self, w, r, r_w_bias, r_r_bias, attn_mask=None, mems=None):
+        qlen, rlen, bsz = w.size(0), r.size(0), w.size(1)
+
+        if mems.size(0) > 0: # TODO: check
+            cat = torch.cat([mems, w], 0)
+            if self.pre_lnorm:
+                w_heads = self.qkv_net(self.layer_norm(cat))
+            else:
+                w_heads = self.qkv_net(cat)
+
+            if mems.dtype == torch.float16:
+                r = r.half() # TODO: should be handled with cfg
+            r_head_k = self.r_net(r)
+
+            w_head_q, w_head_k, w_head_v = torch.chunk(w_heads, 3, dim=-1)
+            w_head_q = w_head_q[-qlen:]
+        else:
+            if self.pre_lnorm:
+                w_heads = self.qkv_net(self.layer_norm(w))
+            else:
+                w_heads = self.qkv_net(w)
+
+            r_head_k = self.r_net(r)
+
+            w_head_q, w_head_k, w_head_v = torch.chunk(w_heads, 3, dim=-1)
+
+        klen = w_head_k.size(0)
+
+        w_head_q = w_head_q.view(qlen, bsz, self.n_head, self.d_head)           # qlen x bsz x n_head x d_head
+        w_head_k = w_head_k.view(klen, bsz, self.n_head, self.d_head)           # klen x bsz x n_head x d_head
+        w_head_v = w_head_v.view(klen, bsz, self.n_head, self.d_head)           # klen x bsz x n_head x d_head
+
+        r_head_k = r_head_k.view(rlen, self.n_head, self.d_head)                # qlen x n_head x d_head
+
+        #### compute attention score
+        rw_head_q = w_head_q + r_w_bias                                         # qlen x bsz x n_head x d_head
+        AC = torch.einsum('ibnd,jbnd->ijbn', (rw_head_q, w_head_k))             # qlen x klen x bsz x n_head
+
+        rr_head_q = w_head_q + r_r_bias
+        BD = torch.einsum('ibnd,jnd->ijbn', (rr_head_q, r_head_k))              # qlen x klen x bsz x n_head
+        BD = self._rel_shift(BD)
+
+        # [qlen x klen x bsz x n_head]
+        attn_score = AC + BD
+        attn_score.mul_(self.scale)
+
+        #### compute attention probability
+        if attn_mask is not None and attn_mask.any().item():
+            if attn_mask.dim() == 2:
+                attn_score = attn_score.float().masked_fill(
+                    attn_mask[None,:,:,None], -float("inf")).type_as(attn_score)
+            elif attn_mask.dim() == 3:
+                attn_score = attn_score.float().masked_fill(
+                    attn_mask[:,:,:,None], -float("inf")).type_as(attn_score)
+
+        # [qlen x klen x bsz x n_head]
+        attn_prob = F.softmax(attn_score, dim=1)
+
+        #### compute attention vector
+        attn_vec = torch.einsum('ijbn,jbnd->ibnd', (attn_prob, w_head_v))
+
+        # [qlen x bsz x n_head x d_head]
+        attn_vec = attn_vec.contiguous().view(
+            attn_vec.size(0), attn_vec.size(1), self.n_head * self.d_head)
+
+        ##### linear projection
+        attn_out = self.o_net(attn_vec)
+
+        if self.pre_lnorm:
+            ##### residual connection
+            # modified. applying ReLU before residual connection
+            output = w + F.relu(attn_out)
+        else:
+            ##### residual connection + layer normalization
+            output = self.layer_norm(w + attn_out)
+
+        return output
+
+
+class MemTransformerLM(nn.Module):
+    def __init__(self, cfg, n_layer, n_head, d_model, d_head, d_inner,
+                 mem_len=1, pre_lnorm=False):
+        super(MemTransformerLM, self).__init__()
+        self.cfg = cfg
+
+        self.d_embed = d_model
+        self.d_model = d_model
+        self.n_head = n_head
+        self.d_head = d_head
+
+        self.n_layer = n_layer
+
+        self.mem_len = mem_len
+
+        self.layers = nn.ModuleList()
+
+        for i in range(n_layer):
+            self.layers.append(
+                RelPartialLearnableDecoderLayer(
+                    n_head, d_model, d_head, d_inner,
+                    mem_len=mem_len, pre_lnorm=pre_lnorm)
+            )
+
+        # create positional encoding-related parameters
+        self.pos_emb = PositionalEmbedding(self.d_model)
+        self.r_r_bias = nn.Parameter(torch.Tensor(self.n_head, self.d_head))
+        self.r_w_bias = nn.Parameter(torch.Tensor(self.n_head, self.d_head))
+
+        self.apply(self.initialize)
+
+    def initialize(self, layer):
+        def init_weight(weight):
+            nn.init.normal_(weight, 0.0, 0.02)  # args.init_std)
+
+        def init_bias(bias):
+            nn.init.constant_(bias, 0.0)
+
+        classname = layer.__class__.__name__
+        if classname.find('Linear') != -1:
+            if hasattr(layer, 'weight') and layer.weight is not None:
+                init_weight(layer.weight)
+            if hasattr(layer, 'bias') and layer.bias is not None:
+                init_bias(layer.bias)
+        elif classname.find('AdaptiveEmbedding') != -1:
+            if hasattr(layer, 'emb_projs'):
+                for i in range(len(layer.emb_projs)):
+                    if layer.emb_projs[i] is not None:
+                        nn.init.normal_(layer.emb_projs[i], 0.0, 0.01)  # args.proj_init_std)
+        elif classname.find('Embedding') != -1:
+            if hasattr(layer, 'weight'):
+                init_weight(layer.weight)
+        elif classname.find('ProjectedAdaptiveLogSoftmax') != -1:
+            if hasattr(layer, 'cluster_weight') and layer.cluster_weight is not None:
+                init_weight(layer.cluster_weight)
+            if hasattr(layer, 'cluster_bias') and layer.cluster_bias is not None:
+                init_bias(layer.cluster_bias)
+            if hasattr(layer, 'out_projs'):
+                for i in range(len(layer.out_projs)):
+                    if layer.out_projs[i] is not None:
+                        nn.init.normal_(layer.out_projs[i], 0.0, 0.01)  # args.proj_init_std)
+        elif classname.find('LayerNorm') != -1:
+            if hasattr(layer, 'weight'):
+                nn.init.normal_(layer.weight, 1.0, 0.02)  # args.init_std)
+            if hasattr(layer, 'bias') and layer.bias is not None:
+                init_bias(layer.bias)
+        elif classname.find('TransformerLM') != -1:
+            if hasattr(layer, 'r_emb'):
+                init_weight(layer.r_emb)
+            if hasattr(layer, 'r_w_bias'):
+                init_weight(layer.r_w_bias)
+            if hasattr(layer, 'r_r_bias'):
+                init_weight(layer.r_r_bias)
+            if hasattr(layer, 'r_bias'):
+                init_bias(layer.r_bias)
+
+    def get_core_out_size(self):
+        return self.d_model
+
+    def init_mems(self):
+        if self.mem_len > 0:
+            mems = []
+            param = next(self.parameters())
+            for i in range(self.n_layer + 1):
+                empty = torch.empty(0, dtype=param.dtype, device=param.device)
+                mems.append(empty)
+
+            return mems
+        else:
+            return None
+
+    def _update_mems(self, hids, mems, mlen, qlen):
+        # does not deal with None
+        if mems is None: return None
+
+        # mems is not None
+        assert len(hids) == len(mems), 'len(hids) != len(mems)'
+
+        with torch.no_grad():
+            new_mems = []
+            end_idx = mlen + max(0, qlen)
+            beg_idx = max(0, end_idx - self.mem_len)
+
+            for i in range(len(hids)):
+
+                cat = torch.cat([mems[i], hids[i]], dim=0)
+                new_mems.append(cat[beg_idx:end_idx].detach())
+
+        # only return last step mem
+        new_mems = [m[-1] for m in new_mems]
+        new_mems = torch.cat(new_mems, dim=-1)
+
+        return new_mems
+
+    def _forward(self, obs_emb, mems=None, mem_begin_index=None, dones=None, from_learner=False):
+        qlen, bsz, _ = obs_emb.size()
+
+        mlen = mems[0].size(0) if mems is not None else 0
+
+        klen = mlen + qlen
+
+        dec_attn_mask = (torch.triu(
+             obs_emb.new_ones(qlen, klen), diagonal=1+mlen)
+            + torch.tril(
+             obs_emb.new_ones(qlen, klen), diagonal=-1)).bool().unsqueeze(-1).repeat(1, 1, bsz)
+
+        for b in range(bsz):
+            dec_attn_mask[:, :(mlen - max(0, mem_begin_index[b])), b] = True
+            if dones is not None:
+                query_done_index = torch.where(dones[:, b] > 0)
+                for q in query_done_index[0]:
+                    # Going to mask out elements before done for new episode
+                    dec_attn_mask[q + 1:, :(mlen + q + 1), b] = True
+
+        hids = []
+        pos_seq = torch.arange(klen-1, -1, -1.0, device=obs_emb.device,
+                               dtype=obs_emb.dtype)
+
+        pos_emb = self.pos_emb(pos_seq)
+        core_out = obs_emb
+
+        hids.append(core_out)
+
+        for i, layer in enumerate(self.layers):
+            mems_i = None if mems is None else mems[i]
+            core_out = layer(core_out, pos_emb, self.r_w_bias,
+                    self.r_r_bias, dec_attn_mask=dec_attn_mask, mems=mems_i)
+            hids.append(core_out)
+
+        new_mems = self._update_mems(hids, mems, mlen, qlen) if not from_learner else None
+
+        return core_out, new_mems
+
+    def forward(self, data, mems, mem_begin_index=None, dones=None, from_learner=False):
+        if mems is None:
+            mems = self.init_mems()
+        else:
+            mems = torch.split(mems, self.d_model, dim=-1)
+
+        if from_learner:
+            data = data.reshape(
+                int(self.cfg.optim.batch_size // self.cfg.optim.rollout),
+                self.cfg.optim.rollout,
+                -1).transpose(0, 1)
+        else:
+            data = data.unsqueeze(0)
+
+        # input observation should be either (1 x B x dim) or (T x B x dim)
+        hidden, new_mems = self._forward(data, mems=mems, mem_begin_index=mem_begin_index, dones=dones,
+                                         from_learner=from_learner)
+
+        # reshape hidden: T x B x dim -> TB x dim
+        hidden = hidden.transpose(0, 1).reshape(hidden.size(0) * hidden.size(1), -1)
+
+        return hidden, new_mems
+
+    def get_mem_begin_index(self, mems_dones, actor_env_step):
+        # mems_dones: (n_batch, n_seq, 1)
+        # actor_env_step: (n_batch)
+        assert mems_dones.shape[0] == actor_env_step.shape[0], (
+            f'The number of batches should be same for mems_done ({mems_dones.shape[0]})'
+            + f' and actor_env_step ({actor_env_step.shape[0]})'
+        )
+        mems_dones = mems_dones.squeeze(-1).cpu()
+        actor_env_step = actor_env_step.cpu()
+
+        arange = torch.arange(1, self.cfg.model.core.mem_len + 1, 1).unsqueeze(0)  # 0 ~ self.cfg.mem_len - 1, (1, n_seq)
+        step_count_dones = mems_dones * arange  # (n_batch, n_seq)
+        step_count_last_dones = step_count_dones.max(dim=-1).values  # (n_batch)
+        numel_to_be_attentioned = self.cfg.model.core.mem_len - step_count_last_dones
+        mem_begin_index = torch.min(numel_to_be_attentioned, actor_env_step)
+        mem_begin_index = mem_begin_index.int().tolist()
+
+        return mem_begin_index
